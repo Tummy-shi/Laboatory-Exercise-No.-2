60aa9db9d17a268284313b6a639a4267f0366f4b
ushwa24 <qjdavardeleon@tip.edu.ph>
2024-08-27 21:36:43 +0800

    Add files via upload

---
+++ DynamicArray.java
@@ -0,0 +1,58 @@
+public class DynamicArray<T> {
+    private Object[] array;
+    private int size;
+    private static final int INITIAL_CAPACITY = 10;
+
+    public DynamicArray() {
+        array = new Object[INITIAL_CAPACITY];
+        size = 0;
+    }
+
+    public void add(T element) {
+        ensureCapacity();
+        array[size++] = element;
+    }
+
+    public void insert(int index, T element) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        ensureCapacity();
+        System.arraycopy(array, index, array, index + 1, size - index);
+        array[index] = element;
+        size++;
+    }
+
+    public void remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        System.arraycopy(array, index + 1, array, index, size - index - 1);
+        array[--size] = null; // Clear the reference
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of bounds");
+        }
+        return (T) array[index];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private void ensureCapacity() {
+        if (size == array.length) {
+            int newCapacity = array.length * 2;
+            Object[] newArray = new Object[newCapacity];
+            System.arraycopy(array, 0, newArray, 0, size);
+            array = newArray;
+        }
+    }
+}
+
