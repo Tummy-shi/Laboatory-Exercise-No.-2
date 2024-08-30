10d30c3f4b819802407123ebad26d3e06fb7526c
akioo27 <rheavencastino123@gmail.com>
2024-08-30 06:07:39 -0700

    Update DynamicArray.java

--- DynamicArray.java
+++ DynamicArray.java
@@ -39,9 +39,7 @@ public class DynamicArray<T> {      //cj
         return (T) array[index];
     } //mem4
 
-    public int size() { //mem2
-        return size;
-    } //mem2
+    
                          //Jason
 
     public boolean isEmpty() { //mem4    
